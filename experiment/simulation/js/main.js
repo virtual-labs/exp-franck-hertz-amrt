95b952610c523eb13b2fb843bf0baded35075dd1
Saneesh P F <41299081+saneeshpfrancis@users.noreply.github.com>
2025-06-05 15:11:14 +0530

    Update main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,11 @@
 //Your JavaScript goes in here
+function copyLink() {
+    const text = document.getElementById("copyText").textContent;
+    navigator.clipboard.writeText(text).then(() => {
+      const tooltip = document.getElementById("tooltip");
+      tooltip.classList.add("show");
+      setTimeout(() => {
+        tooltip.classList.remove("show");
+      }, 1500);
+    });
+  }
